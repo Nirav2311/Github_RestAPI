aa49786d41fd5ffd888a1c82382aea28fd3c9d48
Nirav M Sorathia <55002244+Nirav2311@users.noreply.github.com>
2023-08-10 15:56:26 -0400

    Update constants.js

--- Desktop/Nirav/SpeerApp/components/constants.js
+++ Desktop/Nirav/SpeerApp/components/constants.js
@@ -1,4 +1,4 @@
 //Your Github Token here. I have changed the token for security reason.
-export const GITHUB_AUTH_TOKEN = 'Bearer ghp_tyCRX3SSRk0ErwMdQgzj2Ej2gVT08FJFR';
+export const GITHUB_AUTH_TOKEN = 'Bearer GithubToken';
 export const GET_PROFILE_URL='https://api.github.com/users/';
 export const GET_FOLLOWING_URL = 'https://api.github.com/users/##_LOGIN_##/following';
